5c2f47c2f3dcfa22180c59d950b29d81bbf3546a
dalbano96 <dalbano96@gmail.com>
2015-07-12 14:06:45 -1000

    07/12/15-2

--- Pololu VNH5019 Directional Movement.c
+++ Pololu VNH5019 Directional Movement.c
@@ -10,22 +10,24 @@
  
  This example code is part of the public domain 
  */
-int STBY = 10; //standby
-int Speed = 255; //PWM input 0 to 255
+int STBY = 10;  //Standby
+int Speed;  //Current speed
+int maxSpeed = 80;  //Adjust forward and backward speed
+int maxTurnSpeed = 127;  //Adjust left and right turn speed
 
-//Motor A, BOARD 1
+//Motor A, BOARD 1, Front Left
 int PWMA = 2; //Speed control 
 int AIN1 = 52; //Direction
 int AIN2 = 53; //Direction
-//Motor B, BOARD 1
+//Motor B, BOARD 1, Front Right
 int PWMB = 3; // Speed Control 
 int BIN1 = 50; // Direction 
 int BIN2 = 51; // Direction 
-//Motor C, BOARD 2
+//Motor C, BOARD 2, Back Left
 int PWMC = 4; //Speed control 
 int CIN1 = 22; //Direction
 int CIN2 = 23; //Direction
-//Motor D, BOARD 2
+//Motor D, BOARD 2, Back Right
 int PWMD = 5; // Speed Control 
 int DIN1 = 24; // Direction 
 int DIN2 = 25; // Direction
@@ -59,23 +61,25 @@ void loop()
   {
     int direction = Serial.read();
     
-    //Forward movement
+    //Move Forward
     if(direction == 'w')
     {
 		stop();
-		for(Speed == 0; Speed < 255; Speed++)
+		for(Speed == 0; Speed < maxSpeed; Speed++)
 		{
 		  digitalWrite(STBY, HIGH);
 		  
-		  //Front motors
+		  //Front Left
 		  digitalWrite(AIN1, HIGH);
 		  digitalWrite(AIN2, LOW);
+                  //Front Right
 		  digitalWrite(BIN1, HIGH);
 		  digitalWrite(BIN2, LOW);
 		  
-		  //Back motors
+		  //Back Left
 		  digitalWrite(CIN1, HIGH);
 		  digitalWrite(CIN2, LOW);
+                  //Back Right
 		  digitalWrite(DIN1, HIGH);
 		  digitalWrite(DIN2, LOW);
 		  
@@ -87,23 +91,25 @@ void loop()
 		}
     }
     
-    //Backward movement
+    //Move backward
     if(direction == 's')
     {
 		stop();
-		for(Speed == 0; Speed < 255; Speed++)
+		for(Speed == 0; Speed < maxSpeed; Speed++)
 		{
 		  digitalWrite(STBY, HIGH);
 		  
-		  //Front motors
+		  //Front Left
 		  digitalWrite(AIN1, LOW);
 		  digitalWrite(AIN2, HIGH);
+                  //Front Right
 		  digitalWrite(BIN1, LOW);
 		  digitalWrite(BIN2, HIGH);
 		  
-		  //Back motors
+		  //Back Left
 		  digitalWrite(CIN1, LOW);
 		  digitalWrite(CIN2, HIGH);
+                  //Back Right
 		  digitalWrite(DIN1, LOW);
 		  digitalWrite(DIN2, HIGH);
 		  
@@ -119,21 +125,23 @@ void loop()
     if(direction == 'a')
     {
 		stop();
-		for(Speed == 0; Speed < 255; Speed++)
+		for(Speed == 0; Speed < maxTurnSpeed; Speed++)
 		{
 		  digitalWrite(STBY, HIGH);
 		  
-		  //Front motors
-		  digitalWrite(AIN1, HIGH);
-		  digitalWrite(AIN2, LOW);
-		  digitalWrite(BIN1, LOW);
-		  digitalWrite(BIN2, HIGH);
+		  //Front Left
+		  digitalWrite(AIN1, LOW);
+		  digitalWrite(AIN2, HIGH);
+                  //Front Right
+		  digitalWrite(BIN1, HIGH);
+		  digitalWrite(BIN2, LOW);
 		  
-		  //Back motors
-		  digitalWrite(CIN1, HIGH);
-		  digitalWrite(CIN2, LOW);
-		  digitalWrite(DIN1, LOW);
-		  digitalWrite(DIN2, HIGH);
+		  //Back Left
+		  digitalWrite(CIN1, LOW);
+		  digitalWrite(CIN2, HIGH);
+                  //Back Right
+		  digitalWrite(DIN1, HIGH);
+		  digitalWrite(DIN2, LOW);
 		  
 		  analogWrite(PWMA, Speed);
 		  analogWrite(PWMB, Speed);
@@ -147,21 +155,23 @@ void loop()
     if(direction == 'd')
     {
 		stop();
-		for(Speed == 0; Speed < 255; Speed++)
+		for(Speed == 0; Speed < maxTurnSpeed; Speed++)
 		{
 		  digitalWrite(STBY, HIGH);
 			
-		  //Front motors
-		  digitalWrite(AIN1, LOW);
-		  digitalWrite(AIN2, HIGH);
-		  digitalWrite(BIN1, HIGH);
-		  digitalWrite(BIN2, LOW);
+		  //Front Left
+		  digitalWrite(AIN1, HIGH);
+		  digitalWrite(AIN2, LOW);
+                  //Front Right
+		  digitalWrite(BIN1, LOW);
+		  digitalWrite(BIN2, HIGH);
 			
-		  //Back motors
-		  digitalWrite(CIN1, LOW);
-		  digitalWrite(CIN2, HIGH);
-		  digitalWrite(DIN1, HIGH);
-		  digitalWrite(DIN2, LOW);
+		  //Back Left
+		  digitalWrite(CIN1, HIGH);
+		  digitalWrite(CIN2, LOW);
+                  //Back Right
+		  digitalWrite(DIN1, LOW);
+		  digitalWrite(DIN2, HIGH);
 			
 		  analogWrite(PWMA, Speed);
 		  analogWrite(PWMB, Speed);
@@ -180,7 +190,7 @@ void loop()
 }
 void stop(){
 //enable standby  
-	for(Speed == 255; Speed > 0; Speed--)
+	for(Speed == maxSpeed; Speed > 0; Speed--)
 	{
 	 analogWrite(PWMA, Speed);
 	 analogWrite(PWMB, Speed);
