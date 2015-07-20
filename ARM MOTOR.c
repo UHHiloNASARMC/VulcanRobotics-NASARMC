7fd6d828e953a84e50dc772f43f05c1dbca708f0
Daryl Albano <dalbano96@gmail.com>
2015-07-20 10:33:18 -1000

    07/20/15 ARM and WHEELS

--- ARM MOTOR.c
+++ ARM MOTOR.c
@@ -2,20 +2,16 @@
 
 
 const int STBY = 10;  //Standby
-const int button = 2;  //Button pin
-int buttonState = 0;
+//const int button = 2;  //Button pin
+//int buttonState = 0;
 Servo arm;
 Servo armBrake;
-
-int armSpeed;	//Current arm speed
-int forwardArmSpeed = 2000;	//Move arm in positive direction
-int reverseArmSpeed = 1000;	//Move arm in negative direction
 
 
 void setup()
 {
   pinMode(STBY, OUTPUT);
-  pinMode(button, INPUT);
+  //pinMode(button, INPUT);
   arm.attach(9);
   armBrake.attach(8);
   arm.writeMicroseconds(1500);
@@ -31,44 +27,41 @@ void loop()
   if(Serial.available())
   {
     int direction = Serial.read();
-    buttonState = digitalRead(button);
-    if(buttonState == 0)
-    {
+    //buttonState = digitalRead(button);
     
-	    //Move ARM up
-	    if(direction == 'z')
-	    {
-	    	armStop();
-                armBrake.writeMicroseconds(2000);
-                Serial.println("UP");
-	    	arm.writeMicroseconds(2000);
+	//Move ARM up
+	if(direction == 'z')
+	{
+		armStop();
+		armBrake.writeMicroseconds(2000);
+		Serial.println("UP");
+		arm.writeMicroseconds(2000);
 		delay(3000);
 		armStop();
-	    }
-			
-	     //Move ARM down
-	     if(direction == 'c')
-	     {
-	         armStop();
-                 armBrake.writeMicroseconds(1000);
-                 Serial.println("DOWN");
-	         arm.writeMicroseconds(1000);
-		 delay(100);
-		 armStop();
-	     }
-	
-        //ARM STOP
-        if(direction == 'v')
-        {
-  	  armStop();
-          Serial.println("STOP");
-        }
-    }
-    
-    else
-    {
-      armStop();
-    }
+	}
+		
+	 //Move ARM down
+	 if(direction == 'c')
+	 {
+		armStop();
+		armBrake.writeMicroseconds(1000);
+		Serial.println("DOWN");
+		arm.writeMicroseconds(1000);
+		delay(100);
+		armStop();
+	 }
+
+	//ARM STOP
+	if(direction == 'v')
+	{
+		armStop();
+		Serial.println("STOP");
+	}
+
+	else
+	{
+		armStop();
+	}
     
   }
 }
@@ -77,7 +70,7 @@ void loop()
 void armStop()
 {
 	arm.writeMicroseconds(1500);
-        armBrake.writeMicroseconds(1500);
+    armBrake.writeMicroseconds(1500);
 }
 	
 	
