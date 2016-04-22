5afa87fb8199c8867b69e24154806c1bfbc134e9
dalbano96 <dalbano96@gmail.com>
2016-04-21 18:34:14 -1000

    hey

--- ARM and WHEELS.c
+++ ARM and WHEELS.c
@@ -7,88 +7,98 @@
 **/
 
 
-#include<servo.h>
+#include<Servo.h>
 Servo leftWheels;
 Servo rightWheels;
 Servo arm;
+int PWMA = 13;
+int AIN1 = 49;
+int AIN2 = 48;
 
 void setup() {
-	int PWMA = 2;
-	int AIN1 = 52;
-	int AIN2 = 53;
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  leftWheels.attach(10);
+  rightWheels.attach(12);
+  arm.attach(8);
+  leftWheels.writeMicroseconds(1500);
+  rightWheels.writeMicroseconds(1500);
 
-	leftWheels.attach(8);
-	rightWheels.attach(9);
-	arm.attach(10);
-	leftWheels.writeMicroseconds(1500);
-	rightWheels.writeMicroseconds(1500);
-
-	Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 void loop() {
-	if(Serial.available()) {
-		int input = Serial.read();
-		
-		/**
-		 * Move forward
-		**/
-		if(input == 'w') {
-			leftWheels.writeMicroseconds(1650);
-			rightWheels.writeMicroseconds(1650);
-			delay(1000);
-		}
+  if(Serial.available()) {
+    int input = Serial.read();
+    
+    /**
+     * Move forward
+    **/
+    if(input == 'w') {
+      leftWheels.writeMicroseconds(1350);
+      rightWheels.writeMicroseconds(1650);
+      delay(1000);
+    }
 
-		/**
-		 * Turn left
-		**/
-		if(input == 'a') {
-			leftWheels.writeMicroseconds(1350);
-			rightWheels.writeMicroseconds(1350);
-			delay(1000);
-		}
+    /**
+     * Turn left
+    **/
+    if(input == 'a') {
+      leftWheels.writeMicroseconds(1650);
+      rightWheels.writeMicroseconds(1650);
+      delay(1000);
+    }
 
-		/**
-		 * Move backwards
-		**/
-		if(input == 's') {
-			leftWheels.writeMicroseconds(1350);
-			rightWheels.writeMicroseconds(1650);
-			delay(1000);
-		}
+    /**
+     * Move backwards
+    **/
+    if(input == 's') {
+      leftWheels.writeMicroseconds(1650);
+      rightWheels.writeMicroseconds(1350);
+      delay(1000);
+    }
 
-		/**
-		 * Turn right
-		**/
-		if(input == 'd') {
-			leftWheels.writeMicroseconds(1650);
-			rightWheels.writeMicroseconds(1350);
-			delay(1000);
-		}
+    /**
+     * Turn right
+    **/
+    if(input == 'd') {
+      leftWheels.writeMicroseconds(1350);
+      rightWheels.writeMicroseconds(1350);
+      delay(1000);
+    }
 
-		/**
-		 * arm up
-		**/
-		if(input == 'i') {
-			arm.writeMicroseconds(1650);
-			AIN1=HIGH;
-			AIN2=HIGH;
-			delay(500);
-			AIN1=LOW;
-			AIN2=LOW;
-		}
+    /**
+     * arm up
+    **/
+    if(input == 'i') {
+      arm.writeMicroseconds(1750);
+      AIN1=LOW;
+      AIN2=HIGH;
+      delay(500);
+      AIN1=LOW;
+      AIN2=LOW;
+      arm.writeMicroseconds(1500);
+    }
 
-		/**
-		 * arm up
-		**/
-		if(input == 'n') {
-			arm.writeMicroseconds(1350);
-			AIN1=HIGH;
-			AIN2=HIGH;
-			delay(500);
-			AIN1=LOW;
-			AIN2=LOW;
-		}
+    /**
+     * arm down
+    **/
+    if(input == 'n') {
+      arm.writeMicroseconds(1350);
+      AIN1=HIGH;
+      AIN2=HIGH;
+      delay(500);
+      AIN1=LOW;
+      AIN2=LOW;
+      arm.writeMicroseconds(1500);
+    }
+
+    if(input == 'x') {
+      leftWheels.writeMicroseconds(1500);
+      rightWheels.writeMicroseconds(1500);
+    }
+  }
 }
 
 
